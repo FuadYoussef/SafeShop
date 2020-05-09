31f8dc3a62fbc7a0f1caf7c32a54fc442a5a3fcd
Fuad Youssef <fuadmyoussef@gmail.com>
2020-05-09 14:02:29 -0400

    sign up as business or customer

--- app/src/main/java/tech/youssef/safeshop/SignUpActivity.java
+++ app/src/main/java/tech/youssef/safeshop/SignUpActivity.java
@@ -6,16 +6,26 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class SignUpActivity extends AppCompatActivity {
 
+public class SignUpActivity extends AppCompatActivity {
+    boolean isCustomer = true;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_up);
+
     }
 
     public void launchSearch(View view) {
         Intent searchIntent = new Intent(this, SearchActivity.class);
         startActivity(searchIntent);
+    }
+
+    public void launchSignUpCustomer(View view) {
+        setContentView(R.layout.activity_sign_up);
+    }
+
+    public void launchSignUpBusiness(View view) {
+        isCustomer = false;
+        setContentView(R.layout.activity_sign_up);
     }
 }

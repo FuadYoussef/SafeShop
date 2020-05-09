8c8517c30f1f848c3a451fed8db4795365a3e165
Fuad Youssef <fuadmyoussef@gmail.com>
2020-05-09 13:19:43 -0400

    Finished UI for search

--- app/src/main/java/tech/youssef/safeshop/SearchActivity.java
+++ app/src/main/java/tech/youssef/safeshop/SearchActivity.java
@@ -3,6 +3,8 @@ package tech.youssef.safeshop;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class SearchActivity extends AppCompatActivity {
 
@@ -10,6 +12,33 @@ public class SearchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
+        Spinner typeSpinner = (Spinner) findViewById(R.id.typeSpinner);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> typeAdapter = ArrayAdapter.createFromResource(this,
+                R.array.store_type_array, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        typeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        typeSpinner.setAdapter(typeAdapter);
+
+        Spinner locationSpinner = (Spinner) findViewById(R.id.locationSpinner);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> locationAdapter = ArrayAdapter.createFromResource(this,
+                R.array.store_location_array, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        locationAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        locationSpinner.setAdapter(locationAdapter);
+
+        Spinner sortSpinner = (Spinner) findViewById(R.id.sortBySpinner);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> sortAdapter = ArrayAdapter.createFromResource(this,
+                R.array.sort_by_array, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        sortAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        sortSpinner.setAdapter(sortAdapter);
     }
 
     @Override
